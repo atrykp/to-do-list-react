a8502b5e2af1ff8d1f69f84fa3351bcc6ae7bb15
Patryk Plaza <atrykp0@gmail.com>
2021-01-10 05:57:33 +0100

    change state component to function component AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,104 +1,93 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "../style/AddTask.css";
 
-class AddTask extends Component {
-  state = {
-    correct: true,
-    id: 0,
-    taskName: "",
-    priority: false,
-    date: "",
-    active: true,
-  };
-  handleChange = (e) => {
+const AddTask = (props) => {
+  const [correct, setCorrect] = useState(true);
+  const [id, setId] = useState(0);
+  const [taskName, setTaskName] = useState("");
+  const [priority, setPriority] = useState(false);
+  const [date, setDate] = useState("");
+  const [active, setActive] = useState(true);
+
+  const handleChange = (e) => {
     let name = e.target.name;
     let value = e.target.value;
-    if (name === "taskName") this.setState({ [name]: value });
-    if (name === "priority") this.setState({ [name]: !this.state.priority });
-    if (name === "date") this.setState({ [name]: value });
+    if (name === "taskName") setTaskName(value);
+    if (name === "priority") setPriority(priority);
+    if (name === "date") setDate(value);
   };
 
-  handleAddTask = (e) => {
+  const handleAddTask = (e) => {
     e.preventDefault();
-    if (!this.state.taskName || !this.state.date) {
-      return this.setState({ correct: false });
+    if (!taskName || !date) {
+      return setCorrect(false);
     }
-    const currState = this.state;
+
     const task = {
-      id: currState.id,
-      name: currState.taskName,
-      date: currState.date,
-      priority: currState.priority,
-      active: currState.active,
+      id,
+      name: taskName,
+      date,
+      priority,
+      active,
       doneDate: "",
     };
-    this.props.addTask(task);
-
-    this.setState({
-      correct: true,
-      id: this.state.id + 1,
-      taskName: "",
-      priority: false,
-      date: this.currDate,
-    });
+    props.addTask(task);
+    setCorrect(true);
+    setId((prevVal) => prevVal + 1);
+    setTaskName("");
+    setPriority(false);
+    setDate(currDate);
   };
-  getDate() {
-    let currentDate = new Date().toISOString();
-    let time = currentDate.slice(0, 10);
-    this.currDate = time;
-  }
+  let currDate = "";
 
-  componentDidMount() {
-    this.getDate();
-    this.setState({
-      date: this.currDate,
-    });
-  }
+  // const getDate=()=> {
+  //   let currentDate = new Date().toISOString();
+  //   let time = currentDate.slice(0, 10);
+  //   currDate = time;
+  // }
 
-  render() {
-    const alertName = !this.state.correct && !this.state.taskName && (
-      <p>Wypełnij pole żeby dodać zadanie</p>
-    );
-    const alertDate = !this.state.correct && !this.state.date && (
-      <p>Podaj datę zadania</p>
-    );
+  // componentDidMount() {
+  //   this.getDate();
+  //   this.setState({
+  //     date: this.currDate,
+  //   });
+  // }
 
-    return (
-      <form className="addSection" action="" onSubmit={this.handleAddTask}>
+  const alertName = !correct && !taskName && (
+    <p>Wypełnij pole żeby dodać zadanie</p>
+  );
+  const alertDate = !correct && !date && <p>Podaj datę zadania</p>;
+
+  return (
+    <form className="addSection" action="" onSubmit={handleAddTask}>
+      <input
+        type="text"
+        name="taskName"
+        placeholder="dodaj zadanie"
+        onChange={handleChange}
+        value={taskName}
+      />
+
+      <label htmlFor="priority">
         <input
-          type="text"
-          name="taskName"
-          placeholder="dodaj zadanie"
-          onChange={this.handleChange}
-          value={this.state.taskName}
+          name="priority"
+          type="checkbox"
+          id="priority"
+          onChange={handleChange}
+          checked={priority}
         />
+        oznacz jako priorytet
+      </label>
+      {alertName}
 
-        <label htmlFor="priority">
-          <input
-            name="priority"
-            type="checkbox"
-            id="priority"
-            onChange={this.handleChange}
-            checked={this.state.priority}
-          />
-          oznacz jako priorytet
-        </label>
-        {alertName}
+      <br />
 
-        <br />
-
-        <span>Do kiedy zrobić</span>
-        <input
-          type="date"
-          name="date"
-          onChange={this.handleChange}
-          value={this.state.date}
-        />
-        {alertDate}
-        <br />
-        <button>Dodaj</button>
-      </form>
-    );
-  }
-}
+      <span>Do kiedy zrobić</span>
+      <input type="date" name="date" onChange={handleChange} value={date} />
+      {alertDate}
+      <br />
+      <button>Dodaj</button>
+    </form>
+  );
+};
 export default AddTask;
