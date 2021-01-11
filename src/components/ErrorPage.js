38cc44015c34d05a1098b24f9395b1466e0aabff
Patryk Plaza <atrykp0@gmail.com>
2021-01-11 05:59:58 +0100

    add structure and use useHistory hook in ErrorPage.js

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,6 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>error</div>;
+  const history = useHistory();
+
+  const handleClick = () => history.goBack();
+
+  return (
+    <div className="errorPage">
+      <button onClick={handleClick}>previous page</button>
+      <p>page not found</p>
+    </div>
+  );
 };
 export default ErrorPage;
